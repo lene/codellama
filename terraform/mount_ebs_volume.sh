590b87550e8e554a667cf0ad1c87afd200b7e2aa
Lene Preuss <lene.preuss@gmail.com>
2023-08-31 19:03:35 +0200

    update commands run after boot with real-life experiences

--- terraform/mount_ebs_volume.sh
+++ terraform/mount_ebs_volume.sh
@@ -2,7 +2,17 @@
 
 # mount attached EBS drive
 sudo mkdir -p /home/llama
-(sleep 30; sudo mount /dev/xvdf1 /home/llama) &  # wait for attaching process to finish
+(
+  sleep 30  # wait for attaching process to finish
+  #  there seems to be no guarantee that the EBS volume is any ot these
+  if [[ -b /dev/xvdf1 ]]; then
+    sudo mount /dev/xvdf1 /home/llama
+  elif [[ -b /dev/nvme2n1p1 ]]; then
+    sudo mount /dev/nvme2n1p1 /home/llama
+  elif [[ -b /dev/nvme1n1p1 ]]; then
+    sudo mount /dev/nvme1n1p1 /home/llama
+  fi
+) &
 
 # install fish shell
 sudo apt update
@@ -12,4 +22,6 @@ sudo chsh -s /usr/bin/fish "$USER"
 # update system software and install pytorch and dependencies
 sudo apt -y upgrade
 sudo apt -y install python-is-python3 python3-pip awscli ubuntu-drivers-common
+sudo ubuntu-drivers autoinstall
+sudo rm -f /usr/lib/python3.*/EXTERNALLY-MANAGED
 sudo pip install torch torchvision torchaudio
